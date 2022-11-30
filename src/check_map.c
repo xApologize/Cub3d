a356bb2b1378e9421d7bdfe55bc3d3e03f95fcfe
xApologize <jenbenrossignol@hotmail.com>
2022-11-30 14:22:00 -0500

    add function to check map path and extension

---
+++ src/check_map.c
@@ -0,0 +1,47 @@
+#include "cub3d.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/fcntl.h>
+#include <unistd.h>
+
+void	check_map(char *map, t_data *data)
+{
+	int		map_fd;
+	char	*map_read;
+
+	(void) data;
+	map_read = ft_calloc(sizeof(char), 10000);
+	check_extension(map);
+
+}
+
+void	check_extension(char *map)
+{
+	int		length;
+	char	*extension;
+
+	length = ft_strlen(map);
+	extension = ft_substr(map, length - ft_strlen(EXTENSION), length);
+	if (ft_strncmp(extension, EXTENSION, ft_strlen(EXTENSION)) != 0)
+		exit(EXIT_FAILURE);
+}
+
+int check_map_exist(char *map)
+{
+	int	fd;
+
+	fd = open(map, O_DIRECTORY);
+	if (fd > 0)
+	{
+		printf("The map you passed is a directory\n");
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
+	fd = open(map, O_RDONLY);
+	if (fd == -1)
+	{
+		printf("The map you passed does not exist\n");
+		exit(EXIT_FAILURE);
+	}
+	return (fd);
+}
