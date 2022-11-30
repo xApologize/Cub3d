d67f403d868216a022a250385ed90cda76f5624f
xApologize <jenbenrossignol@hotmail.com>
2022-11-30 14:13:36 -0500

    add function and struct to the header

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,7 +1,21 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# define EXTENSION ".cub"
+
 # include "../lib/libft/include/libft.h"
 # include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+typedef struct s_data
+{
+	char	**map;
+} 			t_data;
+
+//check_map.c
+int		check_map_exist(char *map);
+void	check_map(char *map, t_data *data);
+void	check_extension(char *map);
 
 #endif
