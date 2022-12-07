cb583f5f5c085a7c6581e3a956343a6d23984550
xApologize <jenbenrossignol@hotmail.com>
2022-12-07 11:11:19 -0500

    add check to prevent the function to return null

--- lib/libft/src/ft_strjoinfree.c
+++ lib/libft/src/ft_strjoinfree.c
@@ -6,19 +6,22 @@
 /*   By: jrossign <jrossign@student.42quebec.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 15:18:35 by jrossign          #+#    #+#             */
-/*   Updated: 2022/12/06 15:58:57 by jrossign         ###   ########.fr       */
+/*   Updated: 2022/12/07 11:10:40 by jrossign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libft.h"
-#include <stdlib.h>
 
 char	*ft_strjoinfree(const char *s1, const char *s2)
 {
 	char	*new_line;
 
-	new_line = ft_strjoin(s1, s2);
-	free((char *)s1);
+	if (!s1)
+		new_line = ft_strdup(s2);
+	else
+		new_line = ft_strjoin(s1, s2);
+	if (s1)
+		free((char *)s1);
 	free((char *)s2);
 	return (new_line);
 }
