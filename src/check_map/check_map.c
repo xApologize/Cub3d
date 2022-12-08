b7229050c79cb645abe028621413e38904a6ae1b
xApologize <jenbenrossignol@hotmail.com>
2022-12-08 11:14:04 -0500

    move function to different file

--- src/check_map/check_map.c
+++ src/check_map/check_map.c
@@ -1,26 +1,10 @@
 #include "cub3d.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 void	check_map(char *map, t_data *data)
 {
 	data->map = NULL;
 	check_extension(map);
 	get_map(check_map_exist(map), data);
-}
-
-void	check_extension(char *map)
-{
-	int		length;
-	char	*extension;
-
-	length = ft_strlen(map);
-	extension = ft_substr(map, length - ft_strlen(EXTENSION), length);
-	if (ft_strncmp(extension, EXTENSION, ft_strlen(EXTENSION)) != 0)
-	{
-		printf("Error extension\n");
-		exit(EXIT_FAILURE);
-	}
 }
 
 void	get_map(int map_fd, t_data *data)
@@ -41,24 +25,4 @@ void	get_map(int map_fd, t_data *data)
 	}
 	data->map = ft_split(map, '\n');
 	free(map);
-}
-
-int check_map_exist(char *map)
-{
-	int	fd;
-
-	fd = open(map, O_DIRECTORY);
-	if (fd > 0)
-	{
-		printf("The map you passed is a directory\n");
-		close(fd);
-		exit(EXIT_FAILURE);
-	}
-	fd = open(map, O_RDONLY);
-	if (fd == -1)
-	{
-		printf("The map you passed does not exist\n");
-		exit(EXIT_FAILURE);
-	}
-	return (fd);
 }
