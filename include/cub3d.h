48cdf849d4b66b97480aa8087d3766a6bf0d4152
xApologize <jenbenrossignol@hotmail.com>
2022-12-12 11:25:04 -0500

    add new define for the map extension, new include for limits and the new functinos i createed

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,12 +1,14 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# define EXTENSION ".cub"
+# define EXTENSION_MAP ".cub"
+# define EXTENSION_TEXTURE ".xpm"
 
 # include "../lib/libft/include/libft.h"
 # include <stdio.h>
 # include <stdlib.h>
 # include <fcntl.h>
+# include <limits.h>
 
 typedef struct s_data
 {
@@ -25,18 +27,24 @@ typedef struct s_map
 }			t_map;
 
 //check_arguments.c
-void	check_aguments(t_data *data);
+void	check_arguments(t_data *data);
 void	get_arguments(t_data *data);
 void	set_map_data(t_data *data, char *arg, char *str);
 void	create_map_data(t_data *data);
-int		open_assets_file(char *path);
+int		open_assets_file(char **path);
+
+//check_arguments_1.c
+void	set_colors(t_data *data);
+void	check_colors(t_data *data);
+void	check_isdigit(char **colors);
+void	check_isuchar(char **colors);
 
 //check_map.c
 void	check_map(char *map, t_data *data);
 void	get_map(int map_fd, t_data *data);
 
 //check_path.c
-void	check_extension(char *map);
+void	check_extension(char *path, char *extension);
 int		check_map_exist(char *map);
 
 #endif
