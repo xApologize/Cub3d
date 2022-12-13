d86e9c0915e3cbc73fb941b6ad90ba4f653d09cd
xApologize <jenbenrossignol@hotmail.com>
2022-12-13 11:30:10 -0500

    add flag to functions

--- src/error.c
+++ src/error.c
@@ -1,8 +1,12 @@
 #include "cub3d.h"
 
-void	error_and_free(t_data *data, char *err_msg)
+void	error_and_free(t_data *data, char *err_msg, int flag)
 {
+	ft_putstr_fd(ERROR, 2);
 	ft_putstr_fd(err_msg, 2);
-	free_full_data(data);
+	if (flag == 0)
+		exit(EXIT_FAILURE);
+	else
+		free_full_data(data);
 	exit(EXIT_FAILURE);
 }
