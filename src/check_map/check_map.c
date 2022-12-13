f6508117ae6e55c5e3e0a945748a3a1a466f7b47
xApologize <jenbenrossignol@hotmail.com>
2022-12-13 10:11:34 -0500

    add function to check if the map contain 1 starting point and only valid char

--- src/check_map/check_map.c
+++ src/check_map/check_map.c
@@ -1,6 +1,41 @@
 #include "cub3d.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void	check_map(t_data *data)
 {
 	set_map_only(data);
+	check_map_char(data);
+}
+
+void	check_map_char(t_data *data)
+{
+	int	i;
+	int	j;
+	int	starting_point;
+
+	i = 0;
+	j = 0;
+	starting_point = 0;
+	while (data->map[i])
+	{
+		while (data->map[i][j])
+		{
+			if (!ft_strchr(VALID_MAP_CHAR, data->map[i][j]))
+			{
+				printf("Error check_map_char invalid char: %c\n", data->map[i][j]);
+				exit(EXIT_FAILURE);
+			}
+			if (ft_strchr(VALID_STARTING_POINT, data->map[i][j]))
+				starting_point++;
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	if (starting_point != 1)
+	{
+		printf("Error check_map_char more than 1 starting point.\n");
+		exit(EXIT_FAILURE);
+	}
 }
