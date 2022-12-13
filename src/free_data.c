a6172754fadefccd8de15d931235258a676d8efc
xApologize <jenbenrossignol@hotmail.com>
2022-12-13 11:08:26 -0500

    add files and function to free memory and close fds

---
+++ src/free_data.c
@@ -0,0 +1,28 @@
+#include "cub3d.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+void	free_full_data(t_data *data)
+{
+	if (data->map_data->ceiling_color)
+		free(data->map_data->ceiling_color);
+	if (data->map_data->floor_color)
+		free(data->map_data->floor_color);
+	close_fds(data);
+	if (data->map)
+		ft_freepp((void **) data->map);
+	if (data->map_data)
+		free(data->map_data);
+}
+
+void	close_fds(t_data *data)
+{
+	if (data->map_data->north_wall != 0)
+		close(data->map_data->north_wall);
+	if (data->map_data->south_wall != 0)
+		close(data->map_data->south_wall);
+	if (data->map_data->east_wall != 0)
+		close(data->map_data->east_wall);
+	if (data->map_data->west_wall != 0)
+		close(data->map_data->west_wall);
+}
