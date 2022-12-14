a4109f66049cf0390787e8063748b3bba78b075b
xApologize <jenbenrossignol@hotmail.com>
2022-12-14 12:27:10 -0500

    add function with a flag for different error

--- src/error.c
+++ src/error.c
@@ -10,3 +10,13 @@ void	error_and_free(t_data *data, char *err_msg, int flag)
 		free_full_data(data);
 	exit(EXIT_FAILURE);
 }
+
+void	error_code_arg(t_data *data, int err_code)
+{
+	if (err_code == CODE_PATH_ERR)
+		error_and_free(data, ERR_ARG_PATH, 1);
+	else if (err_code == CODE_EXT_ERR)
+		error_and_free(data, ERR_ASSET_EXT, 1);
+	else
+		error_and_free(data, ERR_ARG_PATH, 1);
+}
