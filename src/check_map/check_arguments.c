0a7efa09f2f060e54704cb9fba7ef04defa8187e
xApologize <jenbenrossignol@hotmail.com>
2022-12-14 12:28:37 -0500

    move function in new files and reducded them

--- src/check_map/check_arguments.c
+++ src/check_map/check_arguments.c
@@ -23,25 +23,16 @@ void	get_arguments(t_data *data)
 
 void	set_map_data(t_data *data, char *arg, char *str)
 {
-	//need to change how this thing work, to have less repetition
-	if (!ft_strncmp(arg, "NO", ft_strlen(arg))
-		&& !data->map_data->north_wall)
-		data->map_data->north_wall = open_assets_file(ft_split(str, ' '), data, arg);
-	else if (!ft_strncmp(arg, "SO", ft_strlen(arg))
-		&& !data->map_data->south_wall)
-		data->map_data->south_wall = open_assets_file(ft_split(str, ' '), data, arg);
-	else if (!ft_strncmp(arg, "EA", ft_strlen(arg))
-		&& !data->map_data->east_wall)
-		data->map_data->east_wall = open_assets_file(ft_split(str, ' '), data, arg);
-	else if (!ft_strncmp(arg, "WE", ft_strlen(arg))
-		&& !data->map_data->west_wall)
-		data->map_data->west_wall = open_assets_file(ft_split(str, ' '), data, arg);
-	else if (!ft_strncmp(arg, "C", ft_strlen(arg))
-		&& !data->map_data->ceiling_color)
-		data->map_data->ceiling_color = set_colors(ft_split(str, ' '));
-	else if (!ft_strncmp(arg, "F", ft_strlen(arg))
-		&& !data->map_data->floor_color)
-		data->map_data->floor_color = set_colors(ft_split(str, ' '));
+	int	fd;
+
+	if (!ft_strcmp(arg, "NO") || !ft_strcmp(arg, "SO")
+			|| !ft_strcmp(arg, "EA") || !ft_strcmp(arg, "WE"))
+	{
+		fd = open_assets_file(ft_split(str, ' '));
+		set_texture(data, arg, fd);
+	}
+	else if (!ft_strcmp(arg, "C") || !ft_strcmp(arg, "F"))
+		set_color(data, arg, str);
 	else
 	{
 		printf("error set_map_data\n");
@@ -51,44 +42,21 @@ void	set_map_data(t_data *data, char *arg, char *str)
 	free(arg);
 }
 
-void	create_map_data(t_data *data)
-{
-	t_map	*map;
-
-	map = malloc(sizeof(t_map));
-	if (!map)
-		error_and_free(data, ERR_MALLOC, 1);
-	map->north_wall = 0;
-	map->south_wall = 0;
-	map->east_wall = 0;
-	map->west_wall = 0;
-	map->ceiling_color = NULL;
-	map->floor_color = NULL;
-	data->map_data = map;
-}
-
-int	open_assets_file(char **path, t_data *data, char *arg)
+int	open_assets_file(char **path)
 {
 	int		fd;
 
 	if (!path[1])
 	{
-		printf("Error open_assets_file: %s\n", path[0]);
-		exit(EXIT_FAILURE);
+		ft_freepp((void **) path);
+		return (CODE_PATH_ERR);
 	}
 	if (check_extension(path[1], EXTENSION_TEXTURE) == -1)
 	{
 		ft_freepp((void **) path);
-		free(arg);
-		error_and_free(data, ERR_ASSET_EXT, 1);
+		return (CODE_EXT_ERR);
 	}
 	fd = open(path[1], O_RDONLY);
-	if (fd < 0)
-	{
-		printf("error open_assets_file: %s\n", path[1]);
-		ft_freepp((void**)path);
-		exit(EXIT_FAILURE);
-	}
 	ft_freepp((void**)path);
 	return (fd);
 }
