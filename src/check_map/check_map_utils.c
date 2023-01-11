5bdb9218fd0ac05d141f871b3d82dfb04dd28659
xApologize <jenbenrossignol@hotmail.com>
2023-01-11 14:31:00 -0500

    add functions to make the map square and fill spaces with 1

--- src/check_map/check_map_utils.c
+++ src/check_map/check_map_utils.c
@@ -6,11 +6,38 @@
 /*   By: jrossign <jrossign@student.42quebec.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 14:17:19 by jrossign          #+#    #+#             */
-/*   Updated: 2022/12/21 14:17:34 by jrossign         ###   ########.fr       */
+/*   Updated: 2023/01/11 14:28:18 by jrossign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+#include <stdlib.h>
+
+int	get_longest_row(t_data *data)
+{
+	int	i;
+	int	longest;
+
+	i = 0;
+	longest = 0;
+	while (data->map[i])
+	{
+		if ((int)ft_strlen(data->map[i]) > longest)
+			longest = (int)ft_strlen(data->map[i]);
+		i++;
+	}
+	return (longest);
+}
+
+char	*get_new_string(char *str, int longest)
+{
+	char	*new_string;
+
+	new_string = ft_calloc(sizeof(char), longest + 1);
+	ft_strlcpy(new_string, str, ft_strlen(str));
+	free(str);
+	return (new_string);
+}
 
 void	set_map_only(t_data *data)
 {
@@ -29,4 +56,33 @@ void	set_map_only(t_data *data)
 	}
 	ft_freepp((void **)data->map);
 	data->map = tmp;
+}
+
+void	set_map_square(t_data *data)
+{
+	int		i;
+	int		longest;
+
+	i = 0;
+	longest = get_longest_row(data);
+	while (data->map[i])
+	{
+		if ((int)ft_strlen(data->map[i]) != longest)
+			data->map[i] = get_new_string(data->map[i], longest);
+		set_space(data->map[i], longest);
+		i++;
+	}
+}
+
+void	set_space(char *new_string, int longest)
+{
+	int	i;
+
+	i = 0;
+	while (i < longest)
+	{
+		if (new_string[i] != '1' && new_string[i] != '0')
+			new_string[i] = '1';
+		i++;
+	}
 }
