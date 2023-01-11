0973845030b3ef8cd1d0e1d6880230e2152f9816
xApologize <jenbenrossignol@hotmail.com>
2023-01-11 08:13:05 -0500

    add comment to make sure we remove this before we push the project and remove old algo test

--- src/flood_fill/flood_fill.c
+++ src/flood_fill/flood_fill.c
@@ -6,12 +6,13 @@
 /*   By: jrossign <jrossign@student.42quebec.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 14:20:12 by jrossign          #+#    #+#             */
-/*   Updated: 2022/12/21 16:26:55 by jrossign         ###   ########.fr       */
+/*   Updated: 2023/01/11 08:10:49 by jrossign         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+//to remove before we push the project
 void	print_map(char **map)
 {
 	int	i;
@@ -55,101 +56,3 @@ void	flood_fill_algo(int x, int y, t_data *data)
 	flood_fill_algo(x + 1, y - 1, data);
 	flood_fill_algo(x - 1, y - 1, data);
 }
-
-// void	flood_fill_algo_in(int x, int y, char **map_copy)
-// {
-	// int	i;
-	//
-	// usleep(90000);
-	// print_map(map_copy);
-	// i = 0;
-	// while (map_copy[i++]);
-	// if (x - 1 >= 0 && map_copy[x - 1][y] == '0')
-	// {
-	// 	map_copy[x - 1][y] = '.';
-	// 	flood_fill_algo_in(x - 1, y, map_copy);
-	// }
-	// if ((size_t)y + 1 < ft_strlen(map_copy[x]) && map_copy[x][y + 1] == '0')
-	// {
-	// 	map_copy[x][y + 1] = '.';
-	// 	flood_fill_algo_in(x, y + 1, map_copy);
-	// }
-	// if (x + 1 < i - 1 && map_copy[x + 1][y] == '0')
-	// {
-	// 	map_copy[x + 1][y] = '.';
-	// 	flood_fill_algo_in(x + 1, y, map_copy);
-	// }
-	// if (y - 1 >= 0 && map_copy[x][y - 1] == '0')
-	// {
-	// 	map_copy[x][y - 1] = '.';
-	// 	flood_fill_algo_in(x, y - 1, map_copy);
-	// }
-	// return ;
-// }
-
-//not useful yet
-// void flood_fill_algo_border(int x, int y, char **map_copy)
-// {
-// 	int	i;
-//
-// 	usleep(90000);
-// 	print_map(map_copy);
-// 	i = 0;
-// 	while (map_copy[i++]);
-// 	if ((x - 1 >= 0) && map_copy[x - 1][y] == '1')
-// 	{
-// 		map_copy[x - 1][y] = 'w';
-// 		flood_fill_algo_border(x - 1, y, map_copy);
-// 	}
-// 	if (((size_t)y + 1 < ft_strlen(map_copy[x])) && map_copy[x][y + 1] == '1')
-// 	{
-// 		map_copy[x][y + 1] = 'w';
-// 		flood_fill_algo_border(x, y + 1, map_copy);
-// 	}
-// 	if ((x + 1 < i - 1) && map_copy[x + 1][y] == '1')
-// 	{
-// 		map_copy[x + 1][y] = 'w';
-// 		flood_fill_algo_border(x + 1, y, map_copy);
-// 	}
-// 	if ((y - 1 >= 0) && map_copy[x][y - 1] == '1')
-// 	{
-// 		map_copy[x][y - 1] = 'w';
-// 		flood_fill_algo_border(x, y - 1, map_copy);
-// 	}
-// 	return ;
-// }
-
-//not working
-// void	check_map_x(char **map)
-// {
-// 	int	x;
-// 	int	y;
-// 	int	len;
-//
-// 	x = 0;
-// 	y = 0;
-// 	len = 0;
-// 	while (map[len++]);
-// 	while (map[y])
-// 	{
-// 		while (map[y][x])
-// 		{
-// 			if (map[y][x] == 'x')
-// 			{
-// 				if ((x == 0) || (x == (int)ft_strlen(map[y])))
-// 				{
-// 					printf("error\n");
-// 					exit(EXIT_FAILURE);
-// 				}
-// 				if ((y == 0) || (y == len))
-// 				{
-// 					printf("error\n");
-// 					exit(EXIT_FAILURE);
-// 				}
-// 			}
-// 			x++;
-// 		}
-// 		x = 0;
-// 		y++;
-// 	}
-// }
